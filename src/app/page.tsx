44452f99afd42f3c4cbec87c7a77dea7760490d8
Harsh Kumar <harsh_kumar@srmap.edu.in>
2026-06-20 03:48:12 +0530

    Fix Vercel static build timeout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { syncUserAndGetWallet } from "@/lib/actions";
 import DashboardUI from "@/components/DashboardUI";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   // Try to sync the user if they are logged in
   const data = await syncUserAndGetWallet();
